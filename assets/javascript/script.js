5950e83c3edd942c4ca5cd0b680897e153612410
Claude Hatfield <r.hatfield269@gmail.com>
2019-09-17 22:08:22 -0500

    have one button working displaying one gif

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,21 +1,21 @@
 $( document ).ready(function() {
   
   // Global Variables 
-  
+  var buttonArray = [];
 
 // Ajax request
 
 $(".button").on("click", function() {
-var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=d7NwVS7qh71bqzCyU6QtjQl8cLGSICEX&tag=poop";
+var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=d7NwVS7qh71bqzCyU6QtjQl8cLGSICEX&q=dog&limit=5";
 
    $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-      
-      var gifImage = response.data.image_original_url;
+      console.log(response);
+      var gifImage = response.data[1].images.original.url;
           var insertGif = $("<img>");
-
+console.log(gifImage);
           // Setting the catImage src attribute to imageUrl
           insertGif.attr("src", gifImage);
           
