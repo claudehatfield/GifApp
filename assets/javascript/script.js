f59be4b5d0f71a11a46d430144743d5f7c25d0e6
Claude Hatfield <r.hatfield269@gmail.com>
2019-09-17 21:41:29 -0500

    ajax call functioning

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,14 +1,37 @@
 $( document ).ready(function() {
   
+  // Global Variables 
+  
 
+// Ajax request
 
-var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=d7NwVS7qh71bqzCyU6QtjQl8cLGSICEX";
+$(".button").on("click", function() {
+var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=d7NwVS7qh71bqzCyU6QtjQl8cLGSICEX&tag=poop";
 
    $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-      console.log(response);
-    });
+      
+      var gifImage = response.data.image_original_url;
+          var insertGif = $("<img>");
 
+          // Setting the catImage src attribute to imageUrl
+          insertGif.attr("src", gifImage);
+          
+
+          // Prepending the catImage to the images div
+          $(".gifArea").prepend(insertGif);
+          console.log('button works');
+    });
   });
+
+ 
+
+
+  
+
+
+
+
+});
