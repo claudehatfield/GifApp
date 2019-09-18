d5194ee32946592e6b86b75593f1aae88b71d04d
Claude Hatfield <r.hatfield269@gmail.com>
2019-09-17 21:20:12 -0500

    added proper link/script tags in html and set up ajax request in javascript

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,14 @@
-var queryUrl =  "https://api.giphy.com/v1/gifs/search?d7NwVS7qh71bqzCyU6QtjQl8cLGSICEX"
+$( document ).ready(function() {
+  
+
+
+var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=d7NwVS7qh71bqzCyU6QtjQl8cLGSICEX";
+
+   $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+    });
+
+  });
