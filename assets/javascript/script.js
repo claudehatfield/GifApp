e1452bba824f4683e71476308d106e48fced8cb1
Claude Hatfield <r.hatfield269@gmail.com>
2019-09-18 21:45:24 -0500

    adding functionality so each button has correct gif response

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,50 +1,59 @@
-$( document ).ready(function() {
-  
+$(document).ready(function () {
+
   // Global Variables 
   var buttonArray = ["The Boondock Saints", "The Terminator", "The Lion King", "Dumb and Dumber", "Pitch Black"];
 
-// Populate the buttons
+  // Populate the buttons
 
- 
-  for (i = 0; i < buttonArray.length; i++){
+
+  for (i = 0; i < buttonArray.length; i++) {
     var gifButton = $("<button>");
-  gifButton.addClass("gifDisplay");
-  gifButton.text(buttonArray[i]);
-  $("#gifArea").append(gifButton);
+    gifButton.addClass("gifDisplay");
+    gifButton.text(buttonArray[i]);
+     gifButton.attr("data-movie", buttonArray[i]);
+    $("#gifButtonArea").append(gifButton);
+    console.log(gifButton);
   }
+  // console.log(gifButton);
+  // Add Ajax request to buttons
 
 
 
 
-// Ajax request
 
 
+  // Ajax request
 
-$(".button").on("click", function() {
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=d7NwVS7qh71bqzCyU6QtjQl8cLGSICEX&q=dog&limit=5";
+  
 
-   $.ajax({
+  $(".gifDisplay").on("click", function () {
+    movies = $(this).attr("data-movie");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=d7NwVS7qh71bqzCyU6QtjQl8cLGSICEX&limit=10&q=" + movies;
+
+    $.ajax({
       url: queryURL,
       method: "GET"
-    }).then(function(response) {
+    }).then(function (response) {
       console.log(response);
-      var gifImage = response.data[2].images.original.url;
-          var insertGif = $("<img>");
-          
-          console.log(gifImage);
-          
-          insertGif.attr("src", gifImage);
-          
-          $(".gifArea").prepend(insertGif);
-          
-          console.log('button works');
+
+
+      var gifImage = response.data.images.original.url;
+      var insertGif = $("<img>");
+
+     for (i = 0; i < )
+
+      insertGif.attr("src", gifImage);
+
+      $(".gifArea").append(insertGif);
+
+      console.log('button works');
     });
   });
 
- 
 
 
-  
+
+
 
 
 
